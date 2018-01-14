56abed22d3ae671b44b564b8dd9e430a186727c7
Allan Liebold <allan.liebold@gmail.com>
2018-01-14 10:27:15 -0800

    added header value to board to leave the top tiles empty, for later use as the game piece spawn area

--- core/Board.cs
+++ core/Board.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class Board : MonoBehaviour {
 
   public Transform m_emptySprite;
-  public int m_height = 30, m_width = 10;
+  public int m_height = 30, m_width = 10, m_header = 8;
 
   void Start() {
 
   }
 
   void DrawEmptyCells() {
-    for(int y = 0; y < m_height; y++) {
+    for(int y = 0; y < m_height - m_header; y++) {
       for(int x = 0; x < m_width; x++) {
         Transform clone;
         clone = Instantiate(m_emptySprite, new Vector3(x, y, 0), Quaternion.identity) as Transform;
