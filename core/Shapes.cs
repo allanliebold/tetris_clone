42c12cfbb8c6f25e36eaea1f88ccc595a9e71d1a
Allan Liebold <allan.liebold@gmail.com>
2018-01-14 14:29:38 -0800

    created move function for shapes, along with move left, right, and down helper functions

--- core/Shapes.cs
+++ core/Shapes.cs
@@ -3,11 +3,27 @@ using System.Collections;
 
 public class Shape : MonoBehaviour {
 
+  void Move(Vector3 moveDirection) {
+    transform.position += moveDirection;
+  }
+
+  public void MoveLeft() {
+    Move(new Vector3(-1, 0, 0));
+  }
+
+  public void MoveRight() {
+    Move(new Vector3(1, 0, 0));
+  }
+
+  public void MoveDown() {
+    Move(new Vector3(0, -1, 0));
+  }
+
   void Start() {
 
   }
 
   void Update() {
-    
+
   }
 }
