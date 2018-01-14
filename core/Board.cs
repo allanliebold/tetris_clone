ce76427d9c2c8778a5d1f2055d4068cac984a680
Allan Liebold <allan.liebold@gmail.com>
2018-01-14 10:12:32 -0800

    added public transform to assign sprite object for tiles and ints for height and width of the 30x10 game board with corresponding values

--- core/Board.cs
+++ core/Board.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class Board : MonoBehaviour {
 
+  public Transform m_emptySprite;
+  public int m_height = 30, m_width = 10;
+
   void Start() {
 
   }
 
   void Update() {
-    
+
   }
 }
