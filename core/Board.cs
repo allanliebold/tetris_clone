6d40c4509d978e9f85e0a5d88e6e919f08c522f4
Allan Liebold <allan.liebold@gmail.com>
2018-01-14 10:14:31 -0800

    created draw empty cells function with a nested for loop to go through each coordinate in the x by y grid

--- core/Board.cs
+++ core/Board.cs
@@ -10,7 +10,11 @@ public class Board : MonoBehaviour {
 
   }
 
-  void Update() {
-
+  void DrawEmptyCells() {
+    for(int y = 0; y < m_height; y++) {
+      for(int x = 0; x < m_width; x++) {
+        
+      }
+    }
   }
 }
