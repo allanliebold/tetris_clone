dbefb4201216c5551c0c4aaefaadf8198dfda1c7
Allan Liebold <allan.liebold@gmail.com>
2018-01-15 09:59:11 -0800

    game controller start assigns the game board and spawner objects and invokes the spawn shape function at the beginning of the game

--- core/GameController.cs
+++ core/GameController.cs
@@ -9,7 +9,18 @@ public class GameController : MonoBehavious {
   Shapes m_activeShape;
 
   void Start() {
+    m_gameBoard = FindObjectOfType<Board>();
+    m_spawner = FindObjectOfType<Spawner>();
 
+    if(m_spawner) {
+      m_spawner.transform.position = Vectorf.Round(m_spawner.transform.position);
+
+      if(m_activeShape == null) {
+        m_activeShape = m_spawner.SpawnShape();
+      }
+    } else {
+      Debug.LogWarning("Warning! No spawner defined");
+    }
   }
 
   void Update() {
