c6119827271ac9878e4a754a587a782606196349
Allan Liebold <allan.liebold@gmail.com>
2018-01-15 09:50:35 -0800

    created game controller script and a placeholder spawner script which will be controlled by it

---
+++ core/GameController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehavious {
+
+  private Board m_gameBoard;
+  private Spawner m_spawner;
+
+  Shapes m_activeShape;
+
+  void Start() {
+
+  }
+
+  void Update() {
+
+  }
+}
